53d5987bce21e5d723aa53e92f741d15e765f50d
Vitalii Koretskyi <wondertalik@gmail.com>
2024-11-03 19:14:00 +0100

    feat: add health check for httpapi

--- src/HttpApi/Program.cs
+++ src/HttpApi/Program.cs
@@ -1,4 +1,4 @@
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -6,8 +6,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks();
 
-var app = builder.Build();
+WebApplication app = builder.Build();
+app.MapHealthChecks("/healthz");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
