84f0e8bfcaf865f8a93a031f8c93c0fac6af325c
Vitalii Koretskyi <wondertalik@gmail.com>
2024-11-16 21:41:05 +0100

    chore: update to azure function 2.0

--- src/Example.FunctionApp1/Program.cs
+++ src/Example.FunctionApp1/Program.cs
@@ -1,14 +1,27 @@
 using Microsoft.Azure.Functions.Worker;
-using Microsoft.Extensions.Hosting;
+using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-IHost host = new HostBuilder()
-    .ConfigureFunctionsWebApplication()
-    .ConfigureServices(services =>
+FunctionsApplicationBuilder builder = FunctionsApplication.CreateBuilder(args);
+builder.ConfigureFunctionsWebApplication();
+
+// Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
+builder.Services
+    .AddApplicationInsightsTelemetryWorkerService()
+    .ConfigureFunctionsApplicationInsights();
+
+builder.Logging.Services.Configure<LoggerFilterOptions>(options =>
+{
+    LoggerFilterRule? defaultRule =
+        options.Rules.FirstOrDefault(rule =>
+            rule.ProviderName == "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
+    
+    if (defaultRule is not null)
     {
-        services.AddApplicationInsightsTelemetryWorkerService();
-        services.ConfigureFunctionsApplicationInsights();
-    })
-    .Build();
+        options.Rules.Remove(defaultRule);
+    }
+});
 
-host.Run();
+await builder.Build().RunAsync();
--- src/Example.FunctionApp1/TimerTriggerOne.cs
+++ src/Example.FunctionApp1/TimerTriggerOne.cs
@@ -9,8 +9,18 @@ public class TimerTriggerOne(ILogger<TimerTriggerOne> logger)
     [Function("TimerTriggerOne")]
     public void Run([TimerTrigger("0 */1 * * * *")] TimerInfo myTimer)
     {
-        logger.LogInformation("C# Timer trigger function executed at: {Now}", DateTime.Now);
+        logger.LogDebug("1_Testing Log Debug");
 
+        logger.LogTrace("1_Testing Log Trace");
+
+        logger.LogInformation("1_Testing Log Information");
+
+        logger.LogWarning("1_Testing Log Warning");
+
+        logger.LogError("1_Testing Log Error");
+
+        logger.LogCritical("1_Testing Log Critical");
+        
         if (myTimer.ScheduleStatus is not null)
         {
             logger.LogInformation("Next timer schedule at: {ScheduleStatusNext}", myTimer.ScheduleStatus.Next);
